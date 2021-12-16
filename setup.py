b733dc5f3d5839d4ab84dbac1fd21de090baec3e
glensk <albert.glensk@gmail.com>
2021-12-16 10:23:03 +0100

    finish setup.py to test with https://colab.research.google.com/

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from distutils.core import setup
-setup(name='glassnode_utils',
+setup(name='crypto-etl-pipeline',
       version='1.0',
-      py_modules=['glassnode_utils'],
+      py_modules=['glassnode_utils','defillama_utils','mongodb_utils'],
       )
